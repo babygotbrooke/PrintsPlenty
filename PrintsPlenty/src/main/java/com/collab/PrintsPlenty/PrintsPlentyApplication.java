fe89229d83362f18d59e68fc2c307cb8fa003562
babygotbrooke <neshamarovinsky@gmail.com>
2022-10-20 18:09:19 -0400

    init new repo

---
+++ PrintsPlenty/src/main/java/com/collab/PrintsPlenty/PrintsPlentyApplication.java
@@ -0,0 +1,13 @@
+package com.collab.PrintsPlenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintsPlentyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintsPlentyApplication.class, args);
+	}
+
+}
---
+++ PrintsPlenty/src/test/java/com/collab/PrintsPlenty/PrintsPlentyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.PrintsPlenty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintsPlentyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
